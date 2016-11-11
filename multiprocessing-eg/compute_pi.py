65b2bb1b9d0f76b5b8008e0a0e376c74abb1bbd2
arnold-jr <j.arnold.111@gmail.com>
2016-11-11 16:32:40 -0600

    Added SLURM batch script

--- multiprocessing-eg/compute_pi.py
+++ multiprocessing-eg/compute_pi.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool
 import numpy as np
 from helpers import CPUS_PER_TASK 
+import argparse
+
 
 def worker_pi(n):
   xy = np.random.rand(n, 2)
@@ -12,7 +14,7 @@ def worker_pi(n):
 def main(n=int(1e4), n_threads=CPUS_PER_TASK):
   print("{0} threads, {1} elements per thread".format(n_threads,n))
 
-  pool = ThreadPool(n_threads)
+  pool = Pool(n_threads)
   result = np.mean(
     pool.map(worker_pi, (n for _ in range(n_threads)))
     )
@@ -23,4 +25,9 @@ def main(n=int(1e4), n_threads=CPUS_PER_TASK):
 
 
 if __name__ == "__main__":
-  print("pi is approximately %0.12f" % main())
+  parser = argparse.ArgumentParser("Compute digits of pi in parallel")
+  parser.add_argument('-n', '--niter', nargs='?', type=int, default=10000,
+      help='number of iterations')
+
+  args = parser.parse_args()
+  print("pi is approximately %0.12f" % main(args.niter))
