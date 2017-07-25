26b5e3b5a875746337808e6c7661f1793dab6d50
frenchwr <will@accre.vanderbilt.edu>
2017-07-25 18:09:46 -0500

    switch almost everything to lmod

--- neural-nets/conda_env.sh
+++ neural-nets/conda_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set the conda package
-setpkgs -a anaconda3
+module load Anaconda3
 
 # Create the conda virtual environment (if not already present)
 conda create -n neural_nets python=3.4 numpy
